9c01628a3a544fac6298a0b6a97724440bc751de
mdblj <thinkjin6@gmail.com>
2024-01-26 14:17:35 +0900

    chore: update contract

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,10 +11,10 @@ type ContractAddresses = {
 export const CONTRACT_ADDRESSES: ContractAddresses = {
   // TODO: handle production
   protocol: {
-    dam: "0x0b6913ca639fEb218a03604211066fD856Aa4990",
-    embankment: "0x20C382f519bBbF1D2BDab2b681Da76F8b6eBcB55",
+    dam: "0x93e407C6A2e3B8fd5BF3103c784bA819C731aAB7",
+    embankment: "0x53F088b44022bd85E952D8D08bc7A06e46A93928",
   },
-  mockYbToken: "0xD299a1bb1b0Cf47353373b4Cc35A512705101354",
+  mockYbToken: "0xe08500d66Fc3fF7e755d316eF52CBf67Fca0269F",
 };
 
 type ToastType = {
